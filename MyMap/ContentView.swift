ea2357a89a5eb89aef95421a87430970bffe2fba
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2021-06-30 16:35:52 +0900

    Initial Commit

---
+++ MyMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyMap
+//
+//  Created by 杉山悠己 on 2021/06/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
